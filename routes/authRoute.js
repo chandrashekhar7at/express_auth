9f444bb4e12faffb885041b646236244ad0cd5f1
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-24 13:10:37 +0530

    asdasd

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,7 +5,7 @@ import { verifyToken } from '../utils/verifyUser.js'
 const router = express()
 
 router.get('/signup',(req,res)=>{
-    res.send("worked")
+    res.send("worked aksjdksad jkas dj")
 })
 router.post('/signup',signup)
 router.post('/signin',signin)
