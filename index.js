d0dd5b952addc169625ffb0e20993f49c5757c39
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-25 16:07:20 +0530

    added proxy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import auth from "./routes/authRoute.js";
@@ -14,32 +13,9 @@ connectDB(DATABASE_URL)
 
 const app = express();
 
-// Set the specific origin of your frontend
-const allowedOrigins = [
-  "https://www.jiospin.info", // Add your frontend URL here
-  // Add more origins if needed
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 
 app.use(cookieParser());
 app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, 'dist')));
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
 
 app.use('/auth', auth);
 
