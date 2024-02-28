5b294fabdd67cfd99832eef56bce44da626c9033
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-28 21:54:38 +0530

    change pass

--- index.js
+++ index.js
@@ -3,22 +3,27 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import auth from "./routes/authRoute.js";
 import connectDB from "./db/connectDB.js";
-import path from 'path';
+import cors from "cors"; // Import cors
 
 dotenv.config();
-const port = process.env.PORT || 8000; 
+const port = process.env.PORT || 8000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
-connectDB(DATABASE_URL)
+connectDB(DATABASE_URL);
 
 const app = express();
-
 
 app.use(cookieParser());
 app.use(express.json());
 
+// Use cors middleware
+app.use(cors({
+  origin:'http://baseergaming.com',
+  credentials:true
+}));
+
 app.use('/auth', auth);
 
-app.listen(port, (req,res) => {
+app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
