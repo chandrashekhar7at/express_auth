d5dd12b1844b2783164244486dafc92781090c77
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-28 23:48:52 +0530

    change pass

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -9,10 +9,10 @@ export const signup = async (req,res)=>{
         const data =new userModel({
             fullname,phone,email,password
         })
-        const resdata = await data.save()
-        if(resdata){
-            res.status(201).json({success:true,message:'user created successfully',data})
-        }
+        // const resdata = await data.save()
+        // if(resdata){
+        //     res.status(201).json({success:true,message:'user created successfully',data})
+        // }
         res.status(200).json({success:false,message:'user created unsuccessfully'})
 
     // try {
