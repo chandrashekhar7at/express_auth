0aca5cb739af00336bf48274fd7a9bbd34740ea5
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-29 14:07:17 +0530

    Resolve conflicts in .env.example, update package-lock.json, and add ecosystem.config.cjs

--- utils/verifyUser.js
+++ utils/verifyUser.js
@@ -7,7 +7,7 @@ export const verifyToken = (req,res,next)=>{
     if(!token){console.log('not valid',token); }
     // res.status(401).json({success:false,message:'not a valid user'})
 
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+    jwt.verify(token,'asdnjuermweas@+_nK_NKNKN++mnxeiru@@#@@#@8763giru23zxasasd',(err,user)=>{
         if(err) {res.status(401).json({success:false,message:'not a valid user'});}
 
         req.user = user
