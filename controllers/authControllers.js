e36f7bea2bcb35f1630ab7c2a032f4584ed78bfc
chandrashekhar7at <chandrashekhar7at@gmail.com>
2024-02-29 13:37:32 +0530

    Resolve conflicts in .env.example, update package-lock.json, and add ecosystem.config.cjs

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -27,7 +27,7 @@ export const signup = async (req,res)=>{
                 secure:true,
                 sameSite:'none',
                 maxAge:expirydate
-            }).status(201).json({success:true,message:'user created successfully',restdata})
+            }).status(201).json({success:true,message:'user created successfully',restdata,token})
         }
     } catch (error) {
         console.log(error.message)
